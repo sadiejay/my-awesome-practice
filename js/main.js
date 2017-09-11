c95ea05e02df881f9d53a0f1473e42dff96719b7
sadiejay <19538219+sadiejay@users.noreply.github.com>
2017-09-12 01:45:05 +0200

    inital commit for week 3

--- js/main.js
+++ js/main.js
@@ -4,3 +4,72 @@ var sadiesInfo = {
     likesTraveling: 'true',
 
 };
+
+
+
+
+
+//////////////////////////////////////////////// JS TYPES
+/************* PRIMITIVES *************/
+
+// strings
+var string1 = "I am a string";
+var string2 = 'This is ok too';
+
+
+
+// numbers
+var num = 0;
+var num1 = 1;
+var num2 = 2;
+var num5 = "2423";// THIS IS A STRING
+
+
+
+
+// boolean
+var bol1 =true;
+var bol2 = false;
+
+
+
+
+// null
+var nulVar = null;
+var nulString = "null";  // THIS IS A STRING
+
+
+
+
+
+
+
+// undefined
+var undefinedVar1 = undefined;
+var undefinedVar2;
+
+
+
+
+
+
+/******************* Complex Types or Object types *******************/
+//Objects
+var myObject = {
+  key: 'value'
+
+
+}
+
+
+//arrays
+var myArray = [1, 2, 3];
+
+var shoppingList = [
+  'Nitro',
+  'bagels',
+  'suaguage',
+]
+
+
+// functions
